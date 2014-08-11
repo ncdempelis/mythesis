07b6d885ec9258232ae56d6faf4cbcaa6ee2c045
ncdempelis <dempelis@gmail.com>
2014-08-11 07:40:50 +0300

    added link to DIR/index.php in login.php

--- project/site/admin/login.php
+++ project/site/admin/login.php
@@ -28,15 +28,15 @@ if(logged_in()) {
 		}
 		?>
 
-<div id="login">
-	<p><?php echo messages();?></p>        
-	<form method="post" action="">
-	<p><label><strong>Username</strong><input type="text" name="username" /></label></p>
-	<p><label><strong>Password</strong><input type="password" name="password" /></label></p>
-	<p><br /><input type="submit" name="submit" class="button" value="login" /></p>                       
-	</form>	  	  
-</div>
-		
+		<div id="login">
+			<p><?php echo messages();?></p>        
+			<form method="post" action="">
+				<p><label><strong>Username</strong><input type="text" name="username" /></label></p>
+				<p><label><strong>Password</strong><input type="password" name="password" /></label></p>
+				<p><br /><input type="submit" name="submit" class="button" value="login" /></p>
+			</form>	  	  
+		</div>
+		<p style="padding-left: 180px;"><a  href="<?php echo DIR ;?>">Αρχική Σελίδα</a></p>
 		</div>	
 		<div class="clear"></div>		
 	</div>

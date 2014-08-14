d2ee9f991105ba67204af4bd739daae33c8f0f50
ncdempelis <dempelis@gmail.com>
2014-08-14 14:09:37 +0300

    corrected footer bar that was extending to the right

--- project/site/admin/footer.php
+++ project/site/admin/footer.php
@@ -1,6 +1,6 @@
-<div id="footer">	
-		<div class="copy">&copy; <?php echo SITETITLE.' '. date('Y');?> </div>
-</div>
-</div>
+	<div id="footer">	
+		<span class="copy" style="margin-left: 10px;">&copy; <?php echo SITETITLE.' '. date('Y');?> </span>
+	</div><!-- footer div -->
+</div><!-- wrapper div -->
 </body>
 </html>
--- project/site/admin/index.php
+++ project/site/admin/index.php
@@ -1,27 +1,11 @@
 <?php 
 require('../includes/config.php'); 
-
-
 include('header.php');
 ?>
-
 <div id="content">
-
-<?php 
-	messages();
-?>
-
 <h1> Κεντρική σελίδα Διαχείρισης</h1>
-
-<p>
-
-
-</p>
-
-</div>
-
+<p><?php messages(); ?></p>
+</div> <!-- content div -->
 <?php
-
 include('footer.php');
-
 ?>
